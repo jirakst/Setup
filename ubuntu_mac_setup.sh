024c2d02d126f9183e6de53d866cf147568846ec
jirakst <jirakst@hotmail.com>
2021-12-12 18:35:36 +0100

    Create ubuntu_mac_setup.sh
    Setting up Ubuntu on a MacBook Pro (mid 2014)

---
+++ ubuntu_mac_setup.sh
@@ -0,0 +1,43 @@
+# MokListRT error
+sudo su -
+cd /boot/efi/EFI/ubuntu
+cp grubx64.efi shimx64.efi
+
+# Internal error
+rm /var/crash/*
+
+# Kinto
+/bin/bash -c "$(wget -qO- https://raw.githubusercontent.com/rbreaves/kinto/HEAD/install/linux.sh || curl -fsSL h>
+
+# Utilities
+sudo apt update
+sudo apt install gnome-tweaks
+sudo apt install git
+sudo apt install vim
+sudo apt install curl
+sudo apt install cpio
+
+# Camera fix
+cd ~/Downloads 
+git clone https://github.com/patjak/facetimehd-firmware.git
+cd facetimehd-firmware
+make clean
+make
+sudo make install
+cd ..
+sudo apt install kmod libssl-dev checkinstall
+git clone https://github.com/patjak/bcwc_pcie.git
+cd bcwc_pcie
+make clean
+make
+sudo make install
+sudo depmod
+sudo modprobe facetimehd
+
+# Applications
+sudo snap install --classic code
+sudo snap install --classic brave
+sudo snap install --classic sublime-text
+
+# Reboot
+reboot
